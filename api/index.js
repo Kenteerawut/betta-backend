372abc6a7e82ce50027bf68d4fc8e21e1f96d5b9
Kenteerawut <compvc20412@gmail.com>
2026-01-19 14:24:35 +0700

    fix: railway start command

--- api/index.js
+++ api/index.js
@@ -1,16 +1,16 @@
 import express from "express";
-import authRoutes from "../routes/auth.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ ok: true });
 });
 
-app.use("/api/auth", authRoutes);
+app.use("/auth", authRoutes);
 
-export default function handler(req, res) {
-  return app(req, res);
-}
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
