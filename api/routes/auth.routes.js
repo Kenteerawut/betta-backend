2915b96ec8007364146b6f8165a66ac52980b241
Kenteerawut <compvc20412@gmail.com>
2026-01-20 14:37:23 +0700

    fix: models user filename casing

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,7 +1,11 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import User from "../../models/user.js";
+
+
+
+
 
 const router = express.Router();
 
