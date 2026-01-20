e273185f6af578b987722805635fbedddd29cdd4
Kenteerawut <compvc20412@gmail.com>
2026-01-20 10:48:01 +0700

    fix routes import path

--- api/index.js
+++ api/index.js
@@ -1,22 +1,14 @@
-import analyzeRoutes from "../routes/analyze.routes.js";
-import authRoutes from "../routes/auth.routes.js";
-import recordRoutes from "../routes/record.routes.js";
-
-
+import express from "express";
+import analyzeRoutes from "./routes/analyze.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import recordRoutes from "./routes/record.routes.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.json({ ok: true, message: "API running on Railway" });
-});
 
 app.use("/api/analyze", analyzeRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/record", recordRoutes);
 
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
-});
+export default app;
